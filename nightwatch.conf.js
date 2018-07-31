58402a136e316e77baff0115c717a8ee7a79eb04
xuchengyin <xuchengyin@uinnova.com>
2018-07-31 12:59:57 +0800

    update

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,20 +1,15 @@
 var os = require('os')
 
-
 module.exports = (function(settings) {
     settings.test_workers = false;
-
     if (os.platform().indexOf('win') == 0) {
-        // settings.test_settings.phantomjs.desiredCapabilities['phantomjs.binary.path'] = './bin/phantomjs-for-linux';
         settings.test_settings.default.desiredCapabilities = settings.test_settings.chrome.desiredCapabilities;
         settings.selenium.start_process = true;
         settings.selenium.cli_args['webdriver.chrome.driver'] = "./bin/2.37/chromedriver.exe";
         settings.test_settings.default.selenium_host = "localhost";
         settings.test_settings.default.launch_url = "http://www.thingjs.com";
-
     } else {
         settings.test_settings.default.launch_url = "http://www.thingjs.com";
     }
-
     return settings;
 })(require('./nightwatch.json'));
