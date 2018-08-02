91542c7bbeb002285954892c69afde7e350e7fb8
xuchengyin <xuchengyin@uinnova.com>
2018-08-02 09:33:53 +0800

    update

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,16 +1,16 @@
 var os = require('os')
 
 module.exports = (function(settings) {
-    // settings.test_workers = false;
-    // if (os.platform().indexOf('win') == 0) {
-    //     settings.test_settings.default.desiredCapabilities = settings.test_settings.chrome.desiredCapabilities;
-    //     settings.selenium.start_process = true;
-    //     settings.selenium.cli_args['webdriver.chrome.driver'] = "./bin/2.37/chromedriver.exe";
-    //     settings.test_settings.default.selenium_host = "localhost";
-    //     settings.test_settings.default.launch_url = "http://www.thingjs.com";
-    // } else {
-    settings.test_settings.default.launch_url = "http://www.thingjs.com";
-    // }
+    settings.test_workers = false;
+    if (os.platform().indexOf('win') == 0) {
+        settings.test_settings.default.desiredCapabilities = settings.test_settings.chrome.desiredCapabilities;
+        settings.selenium.start_process = true;
+        settings.selenium.cli_args['webdriver.chrome.driver'] = "./bin/2.37/chromedriver.exe";
+        settings.test_settings.default.selenium_host = "localhost";
+        settings.test_settings.default.launch_url = "http://www.thingjs.com";
+    } else {
+        settings.test_settings.default.launch_url = "http://www.thingjs.com";
+    }
 
     return settings;
 })(require('./nightwatch.json'));
